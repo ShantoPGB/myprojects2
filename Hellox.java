fbd754d1005b4c7852e3d063cc8df82a859f8266
md shanto 24bd <mshanto055@gmail.com>
2020-10-07 17:03:46 +0600

    Added a new method called display to Hellox.java

--- Hellox.java
+++ Hellox.java
@@ -1,5 +1,11 @@
 public class Hello{
     public static void main(string[]args) {
         system.out.println("vs code is Awesome.....");
+        
+        greeting("HM nayem");
     }
+
+   public static void greeting(string name){
+       system.out.println("welcome " + name);
+   }
 }
