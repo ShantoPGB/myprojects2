4e8f3202a6783a7724604d28feb8bbef1a7a8756
md shanto 24bd <mshanto055@gmail.com>
2020-10-09 20:32:38 +0600

    remove greething and add display mathord

--- Hellox.java
+++ Hellox.java
@@ -2,10 +2,10 @@ public class Hello{
     public static void main(string[]args) {
         system.out.println("vs code is Awesome.....");
         
-        greeting("git");
+        display();
     }
 
-   public static void greeting(string name){
-       system.out.println("Bye Bye " + name);
-   }
+  public static void display() {
+      system.out.println("hallo hallo");
+  }
 }
