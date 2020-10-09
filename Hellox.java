aaf89b27e881dadf1312ac36ada1a99b61f5dec0
md shanto 24bd <mshanto055@gmail.com>
2020-10-09 20:23:58 +0600

    modifi master branch

--- Hellox.java
+++ Hellox.java
@@ -2,10 +2,10 @@ public class Hello{
     public static void main(string[]args) {
         system.out.println("vs code is Awesome.....");
         
-        greeting("git");
+        greeting("shanto ");
     }
 
    public static void greeting(string name){
-       system.out.println("Bye Bye " + name);
+       system.out.println("pgb x " + name);
    }
 }
