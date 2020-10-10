b7cdfb6b3fe3058da6a979b44d0ae16993b34c6b
md shanto 24bd <mshanto055@gmail.com>
2020-10-10 19:25:44 +0600

    delete previous methord and new methord coled sqr

--- Hellox.java
+++ Hellox.java
@@ -2,13 +2,9 @@ public class Hello{
     public static void main(string[]args) {
         system.out.println("vs code is Awesome.....");  
     }
+  
+    public static void sqr(int x) {
+        return x'x;
+    }
 
-   public static void message() {
-       system.out.println("Hooray!!! Now we are able to contribute on open source ...");
-   }
-
-   public static void add(int a, int b) {
-       raturn a+b;
-   }
-
- }
+}
